b967ae0a0afea1b00030e4b0801d6ad7a3f63e9c
caua2006 <caua2006carvalho@gmail.com>
2024-08-05 23:31:39 -0300

    feat: 05/08/2024 23:31

--- src/components/Blur.tsx
+++ src/components/Blur.tsx
@@ -1,7 +1,7 @@
 
 export default function Blur({className}) {
     return (
-      <div className={`size-52 bg-h1Color blur-[92px] rounded-full absolute ${className}`}></div>
+      <div className={`size-52 bg-h1Color blur-[92px] rounded-full absolute ${className} md:size-72 `}></div>
     )
   }
   
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ function Home() {
   return (
     <div>
       <Header/>
-      <div className="flex flex-col items-center gap-3 mb-20 md:flex-row md:items-center">
+      <div className="flex flex-col items-center gap-3 mb-20 md:flex-row md:items-center md:mt-32 md:mb-40">
         <div className="flex flex-col items-center md:w-max md:gap-10 md:justify-between md:items-start">
           <h1 className="text-3xl font-header md:text-6xl">Organize sua saúde com a <span className="text-h1Color">caixa inteligente</span></h1>
           <p className="font-body z-10 text-textBody text-center text-sm md:text-xl md:text-start md:w-80">Descubra como tornamos a gestão de medicamentos mais fácil, segura e independente</p>
@@ -35,14 +35,16 @@ function Home() {
 
       <div className="mb-16">
         {window.innerWidth > 768 ? (
-          <div>
+          <div className="md:flex md:items-center md:justify-center">
+            <div className="flex flex-wrap gap-6 w-[650px]">
             <h2 className="text-2xl font-header md:text-4xl">Não deixe que sua visão atrapalhe o dia a dia</h2>
             <p className="text-textBody text-sm md:text-xl">A caixa que vem para fazer com que você não se esqueça dos seus remédios e nem os tome errado</p>
             <CardHome/>
-            <img src={GirlPhone} alt="" />
+            </div>
+            <img src={GirlPhone} alt="" className="w-[550px]"/>
           </div>
         ) : (
-          <div className="flex flex-col items-center gap-4 ">
+          <div className="flex flex-col items-center gap-4">
             <h2 className="text-2xl font-header">Não deixe que sua visão atrapalhe o dia a dia</h2>
             <p className="text-textBody text-sm">A caixa que vem para fazer com que você não se esqueça dos seus remédios e nem os tome errado</p>
             <img src={GirlPhone} alt="" />
@@ -53,20 +55,26 @@ function Home() {
         <Blur className={'top-[700px]'}/>
       </div>
 
-      <div className="flex flex-col items-center gap-3 mb-16">
-        <h2 className="text-2xl font-header">Explore o poder do nosso app</h2>
-        <p className="text-textBody text-center text-sm">Você já imaginou um mundo onde a gestão dos seus medicamentos seja totalmente acessível e segura, independentemente de qualquer limitação visual? </p>
-        <p className="text-textBody text-center text-sm">Nós acreditamos que todos merecem a tranquilidade de cuidar da saúde de maneira autônoma e independente. É por isso que apresentamos o inovador aplicativo para caixas de remédios, especialmente desenvolvido para pessoas cegas.</p>
+      <div className="flex flex-col items-center gap-3 mb-16 md:flex-row-reverse md:mb-32">
+        <div className="flex flex-col items-center md:w-[550px] md:gap-10">
+          <h2 className="text-2xl font-header md:text-4xl">Explore o poder do nosso app</h2>
+          <p className="text-textBody text-center text-sm md:text-base md:text-justify">Você já imaginou um mundo onde a gestão dos seus medicamentos seja totalmente acessível e segura, independentemente de qualquer limitação visual? 
+            <br /> <br />
+            Nós acreditamos que todos merecem a tranquilidade de cuidar da saúde de maneira autônoma e independente. É por isso que apresentamos o inovador aplicativo para caixas de remédios, especialmente desenvolvido para pessoas cegas.
+          </p>
+          <Button placeholder={'Baixe agora'}/>
+        </div>
+        <img src={MobileAppImg} alt="" className="w-[600px]"/>
         <Blur className={'top-[2250px]'}/>
-        <Button placeholder={'Baixe agora'}/>
-        <img src={MobileAppImg} alt="" />
       </div>
 
-      <div className="flex flex-col items-center gap-3 mb-16">
-        <h2 className="text-2xl font-header text-h1Color">Quem somos </h2>
-        <p className="text-textBody text-center text-sm">Somos um grupo diversificado de profissionais dedicados a transformar a maneira como pessoas cegas e com baixa visão gerenciam sua saúde. Nossa equipe é composta por especialistas em tecnologia, saúde e acessibilidade, unidos por um objetivo comum: promover a autonomia e a inclusão através da inovação.</p>
-        <Button placeholder={'Saiba mais'}/>
-        <img src={BannerPillbox} alt="" className="z-20"/>
+      <div className="flex flex-col items-center gap-3 mb-16 md:flex-row">
+        <div className="flex flex-col items-center md:w-[600px] md:gap-5">
+          <h2 className="text-2xl font-header text-h1Color md:text-4xl">Quem somos </h2>
+          <p className="text-textBody text-center text-sm md:text-base">Somos um grupo diversificado de profissionais dedicados a transformar a maneira como pessoas cegas e com baixa visão gerenciam sua saúde. Nossa equipe é composta por especialistas em tecnologia, saúde e acessibilidade, unidos por um objetivo comum: promover a autonomia e a inclusão através da inovação.</p>
+          <Button placeholder={'Saiba mais'}/>
+        </div>
+        <img src={BannerPillbox} alt="" className="z-20 md:w-[550px] "/>
       </div>
 
       <Footer/>
