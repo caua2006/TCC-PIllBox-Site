cf2649f9333243f695792b811adcab2e26749418
caua2006 <caua2006carvalho@gmail.com>
2024-08-07 22:55:46 -0300

    Home, products, pillbox, pillboxapp done

--- src/pages/PillBox.tsx
+++ src/pages/PillBox.tsx
@@ -87,7 +87,7 @@ export default function PillBox() {
             </div>
 
             <div className="md:w-full md:overflow-auto md:ml-[300px]">
-                <div className="flex flex-col items-center gap-4 mb-5">
+                <div className="flex flex-col  gap-4 mb-5">
                     <h1 className="text-3xl font-header text-h1Color">1 - PillBox - Introdução</h1>
                     <h2 className="text-2xl font-header text-h1Color">Por que criamos a caixa de remédios inteligente?</h2>
                     <p className="text-textBody">A gestão de medicamentos é crucial para a saúde e bem-estar, mas pode ser particularmente desafiadora para pessoas cegas ou com baixa visão. Estudos indicam que a má administração de medicamentos é uma preocupação significativa, com potencial para causar sérias complicações de saúde. A Caixa de Remédios Inteligente foi criada para atender a essa necessidade, oferecendo uma solução que combina acessibilidade, segurança e independência.
@@ -96,7 +96,7 @@ export default function PillBox() {
                     <div className="w-full bg-bgCard h-0.5"></div>
                 </div>
 
-                <div className="flex flex-col items-center gap-4 mb-5">
+                <div className="flex flex-col  gap-4 mb-5">
                     <h1 className="text-3xl font-header text-h1Color">2 - Passo a passo de uso da caixa</h1>
                     <h2 className="text-2xl font-header text-h1Color">Configuração inicial</h2>
                     <p className="text-textBody">Organize seus medicamentos nos compartimentos da caixa. Cada compartimento pode ser etiquetado com informações em braille.
@@ -111,36 +111,41 @@ export default function PillBox() {
                     <div className="w-full bg-bgCard h-0.5"></div>
                 </div>
 
-                <div className="flex flex-col items-center gap-4 mb-5">
-                    <h1 className="text-3xl font-header text-h1Color">2 - Passo a passo de uso da caixa</h1>
-                    <h2 className="text-2xl font-header text-h1Color">Configuração inicial</h2>
-                    <p className="text-textBody">Organize seus medicamentos nos compartimentos da caixa. Cada compartimento pode ser etiquetado com informações em braille.
-                    Conecte a caixa ao aplicativo no seu smartphone via Bluetooth, seguindo as instruções de emparelhamento no app.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Programação de lembretes</h2>
-                    <p className="text-textBody">Use o aplicativo para definir horários e frequência de medicação. Os dados serão enviados para a caixa automaticamente.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Uso diário</h2>
-                    <p className="text-textBody">Nos horários definidos, a caixa emitirá alertas sonoros e táteis para lembrá-lo de tomar os medicamentos.
-                    Abra o compartimento indicado e retire o medicamento. A caixa detectará a abertura e fechamento do compartimento.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Confirmação de uso</h2>
-                    <p className="text-textBody">Após retirar o medicamento, confirme a ação no aplicativo ou simplesmente feche o compartimento para que o sensor de proximidade registre a retirada.</p>
-                    <div className="w-full bg-bgCard h-0.5"></div>
+                <div className="flex flex-col  gap-4 mb-5">
+                    <h1 className="text-3xl font-header text-h1Color">3 - O Aplicativo: facilitando o uso da caixa</h1>
+                    <p className="text-textBody">O aplicativo complementar foi projetado para ser intuitivo e fácil de usar, oferecendo várias funcionalidades que garantem uma gestão eficaz dos medicamentos:</p>
+                    <h2 className="text-2xl font-header text-h1Color">Interface acessível</h2>
+                    <p className="text-textBody">Compatível com os principais leitores de tela, facilitando a navegação por voz.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Lembretes personalizáveis</h2>
+                    <p className="text-textBody">Permite definir horários e frequências de medicação de acordo com necessidades específicas.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Notificações multimodais</h2>
+                    <p className="text-textBody">Receba alertas por som, vibração e notificações no aplicativo, garantindo que o deficiente visual nunca perca uma dose.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Histórico de medicação</h2>
+                    <p className="text-textBody">Monitore seu uso de medicamentos e compartilhe dados com seus profissionais de saúde para um acompanhamento mais preciso.</p>
+                <div className="w-full bg-bgCard h-0.5"></div>
+                
                 </div>
 
-                <div className="flex flex-col items-center gap-4 mb-5">
-                    <h1 className="text-3xl font-header text-h1Color">2 - Passo a passo de uso da caixa</h1>
-                    <h2 className="text-2xl font-header text-h1Color">Configuração inicial</h2>
-                    <p className="text-textBody">Organize seus medicamentos nos compartimentos da caixa. Cada compartimento pode ser etiquetado com informações em braille.
-                    Conecte a caixa ao aplicativo no seu smartphone via Bluetooth, seguindo as instruções de emparelhamento no app.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Programação de lembretes</h2>
-                    <p className="text-textBody">Use o aplicativo para definir horários e frequência de medicação. Os dados serão enviados para a caixa automaticamente.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Uso diário</h2>
-                    <p className="text-textBody">Nos horários definidos, a caixa emitirá alertas sonoros e táteis para lembrá-lo de tomar os medicamentos.
-                    Abra o compartimento indicado e retire o medicamento. A caixa detectará a abertura e fechamento do compartimento.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Confirmação de uso</h2>
-                    <p className="text-textBody">Após retirar o medicamento, confirme a ação no aplicativo ou simplesmente feche o compartimento para que o sensor de proximidade registre a retirada.</p>
+                <div className="flex flex-col gap-4 mb-5 text-left">
+                    <h1 className="text-3xl font-header text-h1Color">4 - Como a caixa foi criada: componentes em arduino</h1>
+                    <p className="text-textBody">A Caixa de Remédios Inteligente foi desenvolvida utilizando a tecnologia Arduino, conhecida por sua versatilidade e precisão. Aqui está um resumo do processo de criação e dos componentes utilizados:</p>
+                    <h2 className="text-2xl font-header text-h1Color">Pesquisa e desenvolvimento</h2>
+                    <p className="text-textBody">Identificação das necessidades dos usuários e avaliação das tecnologias disponíveis para criar uma solução acessível e eficiente.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Prototipagem</h2>
+                    <p className="text-textBody">Desenvolvimento de protótipos utilizando componentes Arduino, que permitiram controlar e monitorar todas as funções da caixa.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Componentes utilizados</h2>
+                    <p className="text-textBody">
+                        - Microcontrolador Arduino: o núcleo do sistema, responsável por gerenciar os sensores e atuadores.<br/>
+                        - Sensores de proximidade: Detectam a abertura e fechamento dos compartimentos. <br />
+                        - Módulo Bluetooth: Permite a comunicação entre a caixa e o aplicativo.<br/>
+                        - Atuadores: Emitem alertas sonoros e táteis para lembrar o usuário a tomar a medicação.
+                    </p>
+                    <h2 className="text-2xl font-header text-h1Color">Testes com usuários</h2>
+                    <p className="text-textBody">Realizados testes com pessoas cegas e com baixa visão para garantir que a caixa e o aplicativo atendem às suas necessidades.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Refinamento e produção final</h2>
+                    <p className="text-textBody">Ajustes e melhorias das funcionalidades com base no feedback dos usuários e produção em escala das caixas.</p>
                     <div className="w-full bg-bgCard h-0.5"></div>
                 </div>
-
 
                 <Footer/>
             </div>
--- src/pages/PillBoxApp.tsx
+++ src/pages/PillBoxApp.tsx
@@ -8,6 +8,9 @@ import {
     SheetTrigger,
   } from "@/components/ui/sheet"
 
+import AppleStore from '/footer/AppleStore.svg'
+import PlayStore from '/footer/PlayStore.svg'
+
 import { ChevronRight } from 'lucide-react';
 export default function PillBoxApp() {
   return (
@@ -58,85 +61,88 @@ export default function PillBoxApp() {
     <main className="md:flex">
             <div className="md:fixed md:max-w-64 hidden md:block">
                 <div className="flex flex-col items-start gap-1 mb-1">
-                    <p className="text-h1Color">1 - PillBox - Introdução</p>
-                    <p className="text-textBody">Por que criamos a caixa de remédios inteligente?</p>
+                    <p className="text-h1Color">1 - PillBoxApp - Introdução</p>
+                    <p className="text-textBody">Por que criamos o aplicativo?</p>
                 </div>
                 <div className="flex flex-col items-start gap-1 mb-1">
-                    <p className="text-h1Color">2 - Passo a passo de uso da caixa</p>
-                    <p className="text-textBody">Configuração inicial</p>
-                    <p className="text-textBody">Programação de lembretes</p>
-                    <p className="text-textBody">Uso diário</p>
-                    <p className="text-textBody">Confirmação de uso</p>
+                    <p className="text-h1Color">2 - Passo a passo de uso do App</p>
+                    <p className="text-textBody">Download e Instalação</p>
+                    <p className="text-textBody">Configuração Inicial</p>
+                    <p className="text-textBody">Cadastro de medicamentos</p>
+                    <p className="text-textBody">Configuração de lembretes</p>
                 </div>
                 <div className="flex flex-col items-start gap-1 mb-1">
-                    <p className="text-h1Color">3- O Aplicativo</p>
-                    <p className="text-textBody">Interface acessível</p>
-                    <p className="text-textBody">Lembretes personalizáveis</p>
-                    <p className="text-textBody">Notificações multimodais</p>
-                    <p className="text-textBody">Histórico de medicação</p>
+                    <p className="text-h1Color">3- Utilizando a caixa com o App</p>
+                    <p className="text-textBody">Sincronização automática</p>
+                    <p className="text-textBody">Alertas multimodais</p>
+                    <p className="text-textBody">Feedback em tempo real</p>
+                    <p className="text-textBody">Assistência por voz</p>
                 </div>
                 <div className="flex flex-col items-start gap-1 mb-1">
-                    <p className="text-h1Color">4 - Como a caixa foi criada</p>
-                    <p className="text-textBody">Pesquisa e desenvolvimento</p>
-                    <p className="text-textBody">Prototipagem</p>
-                    <p className="text-textBody">Componentes utilizados</p>
-                    <p className="text-textBody">Testes com usuários</p>
-                    <p className="text-textBody">Refinamento e produção final</p>
+                    <p className="text-h1Color">4 - Como o App foi criado</p>
+                    <p className="text-textBody">Tecnologias utilizadas</p>
+                    <p className="text-textBody">Banco de Dados</p>
+                    <p className="text-textBody">Conexões e sincronização</p>
                 </div>
             </div>
 
             <div className="md:w-full md:overflow-auto md:ml-[300px]">
-                <div className="flex flex-col items-center gap-4 mb-5">
-                    <h1 className="text-3xl font-header text-h1Color">1 - PillBox - Introdução</h1>
-                    <h2 className="text-2xl font-header text-h1Color">Por que criamos a caixa de remédios inteligente?</h2>
-                    <p className="text-textBody">A gestão de medicamentos é crucial para a saúde e bem-estar, mas pode ser particularmente desafiadora para pessoas cegas ou com baixa visão. Estudos indicam que a má administração de medicamentos é uma preocupação significativa, com potencial para causar sérias complicações de saúde. A Caixa de Remédios Inteligente foi criada para atender a essa necessidade, oferecendo uma solução que combina acessibilidade, segurança e independência.
+                <div className="flex flex-col gap-4 mb-5">
+                    <h1 className="text-3xl font-header text-h1Color">1 - PillBoxApp - Introdução</h1>
+                    <h2 className="text-2xl font-header text-h1Color">Por que criamos o aplicativo?</h2>
+                    <p className="text-textBody">A administração correta dos medicamentos é vital para a saúde, mas para pessoas cegas ou com baixa visão, pode ser uma tarefa desafiadora. PillBoxApp foi criado para transformar essa realidade proporcionando uma ferramenta que não só auxilia na gestão dos medicamentos, mas também integra perfeitamente com a Caixa de Remédios Inteligente. O objetivo é oferecer mais autonomia, segurança e eficiência no cuidado diário com a saúde.
                     <br /> <br />
-                    A PillBox foi desenvolvida após extensa pesquisa e feedback de usuários reais e especialistas em saúde. O objetivo é proporcionar uma ferramenta que elimina as incertezas e dificuldades na administração de medicamentos, garantindo que cada dose seja tomada corretamente e no momento certo.</p>
+                    O aplicativo foi desenvolvido para atender às necessidades específicas de acessibilidade, garantindo que cada dose de medicamento seja tomada corretamente e no momento certo.</p>
                     <div className="w-full bg-bgCard h-0.5"></div>
                 </div>
 
-                <div className="flex flex-col items-center gap-4 mb-5">
-                    <h1 className="text-3xl font-header text-h1Color">2 - Passo a passo de uso da caixa</h1>
+                <div className="flex flex-col gap-4 mb-5">
+                    <h1 className="text-3xl font-header text-h1Color">2 - Passo a passo de uso do App</h1>
+                    <h2 className="text-2xl font-header text-h1Color">Download e instalação:</h2>
+                    <p className="text-textBody"> - Baixe o aplicativo na App Store ou Google Play. <br /> 
+                    - Instale o aplicativo no seu smartphone e abra-o para iniciar a configuração.</p>
+                    <div className="flex gap-4">
+                        <img src={AppleStore} alt="" />
+                        <img src={PlayStore} alt="" />
+                    </div>
                     <h2 className="text-2xl font-header text-h1Color">Configuração inicial</h2>
-                    <p className="text-textBody">Organize seus medicamentos nos compartimentos da caixa. Cada compartimento pode ser etiquetado com informações em braille.
-                    Conecte a caixa ao aplicativo no seu smartphone via Bluetooth, seguindo as instruções de emparelhamento no app.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Programação de lembretes</h2>
-                    <p className="text-textBody">Use o aplicativo para definir horários e frequência de medicação. Os dados serão enviados para a caixa automaticamente.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Uso diário</h2>
-                    <p className="text-textBody">Nos horários definidos, a caixa emitirá alertas sonoros e táteis para lembrá-lo de tomar os medicamentos.
-                    Abra o compartimento indicado e retire o medicamento. A caixa detectará a abertura e fechamento do compartimento.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Confirmação de uso</h2>
-                    <p className="text-textBody">Após retirar o medicamento, confirme a ação no aplicativo ou simplesmente feche o compartimento para que o sensor de proximidade registre a retirada.</p>
+                    <p className="text-textBody">-Siga as instruções na tela para configurar seu perfil e preferências de uso. <br />
+                    - Conecte o aplicativo à Caixa de Remédios Inteligente via Bluetooth, seguindo as instruções de emparelhamento.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Cadastro de medicamentos</h2>
+                    <p className="text-textBody">- Utilize o leitor de código de barras do aplicativo para adicionar seus medicamentos à lista. <br />
+                    - Insira manualmente os horários e doses recomendadas ou importe essas informações de uma prescrição digital.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Configuração de lembretes</h2>
+                    <p className="text-textBody">Defina os horários e frequência de medicação no aplicativo. <br />
+                    Personalize os tipos de notificações que você deseja receber (sonoras, vibrações, etc.).</p>
                     <div className="w-full bg-bgCard h-0.5"></div>
                 </div>
 
-                <div className="flex flex-col items-center gap-4 mb-5">
-                    <h1 className="text-3xl font-header text-h1Color">2 - Passo a passo de uso da caixa</h1>
-                    <h2 className="text-2xl font-header text-h1Color">Configuração inicial</h2>
-                    <p className="text-textBody">Organize seus medicamentos nos compartimentos da caixa. Cada compartimento pode ser etiquetado com informações em braille.
-                    Conecte a caixa ao aplicativo no seu smartphone via Bluetooth, seguindo as instruções de emparelhamento no app.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Programação de lembretes</h2>
-                    <p className="text-textBody">Use o aplicativo para definir horários e frequência de medicação. Os dados serão enviados para a caixa automaticamente.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Uso diário</h2>
-                    <p className="text-textBody">Nos horários definidos, a caixa emitirá alertas sonoros e táteis para lembrá-lo de tomar os medicamentos.
-                    Abra o compartimento indicado e retire o medicamento. A caixa detectará a abertura e fechamento do compartimento.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Confirmação de uso</h2>
-                    <p className="text-textBody">Após retirar o medicamento, confirme a ação no aplicativo ou simplesmente feche o compartimento para que o sensor de proximidade registre a retirada.</p>
+                <div className="flex flex-col  gap-4 mb-5">
+                    <h1 className="text-3xl font-header text-h1Color">3 - Utilizando a caixa com o App</h1>
+                    <p className="text-textBody">O PillBoxApp funciona em perfeita sincronia com a Caixa de Remédios Inteligente, proporcionando uma experiência integrada e segura:</p>
+                    <h2 className="text-2xl font-header text-h1Color">Sincronização automática</h2>
+                    <p className="text-textBody">O aplicativo sincroniza automaticamente com a caixa, garantindo que os lembretes e registros de uso sejam precisos e atualizados.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Alertas multimodais</h2>
+                    <p className="text-textBody">Receba alertas no seu smartphone e na caixa, garantindo que você nunca perca uma dose da medicação.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Feedback em tempo real</h2>
+                    <p className="text-textBody">Quando você abre um compartimento na caixa, o aplicativo registra automaticamente a retirada do medicamento, atualizando seu histórico.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Assistência por voz</h2>
+                    <p className="text-textBody">Utilize comandos de voz para configurar lembretes e verificar informações sobre seus medicamentos.</p>
                     <div className="w-full bg-bgCard h-0.5"></div>
                 </div>
 
-                <div className="flex flex-col items-center gap-4 mb-5">
-                    <h1 className="text-3xl font-header text-h1Color">2 - Passo a passo de uso da caixa</h1>
-                    <h2 className="text-2xl font-header text-h1Color">Configuração inicial</h2>
-                    <p className="text-textBody">Organize seus medicamentos nos compartimentos da caixa. Cada compartimento pode ser etiquetado com informações em braille.
-                    Conecte a caixa ao aplicativo no seu smartphone via Bluetooth, seguindo as instruções de emparelhamento no app.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Programação de lembretes</h2>
-                    <p className="text-textBody">Use o aplicativo para definir horários e frequência de medicação. Os dados serão enviados para a caixa automaticamente.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Uso diário</h2>
-                    <p className="text-textBody">Nos horários definidos, a caixa emitirá alertas sonoros e táteis para lembrá-lo de tomar os medicamentos.
-                    Abra o compartimento indicado e retire o medicamento. A caixa detectará a abertura e fechamento do compartimento.</p>
-                    <h2 className="text-2xl font-header text-h1Color">Confirmação de uso</h2>
-                    <p className="text-textBody">Após retirar o medicamento, confirme a ação no aplicativo ou simplesmente feche o compartimento para que o sensor de proximidade registre a retirada.</p>
+                <div className="flex flex-col gap-4 mb-5">
+                    <h1 className="text-3xl font-header text-h1Color">4 - Como o App foi criado</h1>
+                    <p className="text-textBody">O aplicativo foi desenvolvido utilizando tecnologias avançadas para garantir robustez, segurança e acessibilidade:</p>
+                    <h2 className="text-2xl font-header text-h1Color">Tecnologias utilizadas</h2>
+                    <p className="text-textBody">Linguagens de Programação: Swift (iOS) e Kotlin (Android) para garantir desempenho nativo e otimizado.
+                    Frameworks: Utilização de frameworks, como Flutter, para criar uma interface de usuário acessível e intuitiva.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Banco de Dados</h2>
+                    <p className="text-textBody">Firebase: Para armazenamento seguro e em tempo real das informações dos usuários e histórico de medicação.
+                    SQLite: Banco de dados local no dispositivo para garantir acesso rápido às informações críticas.</p>
+                    <h2 className="text-2xl font-header text-h1Color">Conexões e sincronização</h2>
+                    <p className="text-textBody">Bluetooth Low Energy (BLE): Conexão eficiente e de baixa energia entre o aplicativo e a Caixa de Remédios Inteligente.
+                    APIs RESTful: Comunicação segura e eficiente com servidores remotos, permitindo atualizações de software e sincronização de dados.</p>
                     <div className="w-full bg-bgCard h-0.5"></div>
                 </div>
 
